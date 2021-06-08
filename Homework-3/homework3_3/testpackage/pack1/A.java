719404e2ad416f0d2272c2e3d532541d85e56141
Dozen Prince Li <61978255+dozenprinceli@users.noreply.github.com>
2021-06-08 23:24:11 +0800

    Add files via upload

---
+++ Homework-3/homework3_3/testpackage/pack1/A.java
@@ -0,0 +1,11 @@
+package homework3_3.testpackage.pack1;
+
+public class A {
+    private int i;
+    float f;
+    public double d;
+
+    public A() {
+        
+    }
+}
---
+++ Homework-3/homework3_3/testpackage/pack1/B.java
@@ -0,0 +1,9 @@
+package homework3_3.testpackage.pack1;
+
+public class B {
+    public static void main(String[] args) {
+        A a = new A();
+        a.f = 2;
+        a.d = 3;
+    }
+}
