716c31b1a62fce9dcf9ee1f3a409cf82a14ec073
Dozen Prince Li <61978255+dozenprinceli@users.noreply.github.com>
2021-07-03 08:24:45 +0800

    homework2_6更新至v1.2.210703版本

--- Homework-2/homework2_6/Print.java
+++ Homework-2/homework2_6/Print.java
@@ -1,8 +1,19 @@
 package homework2_6;
 
+/**
+ * Print类。用于输出大小写英文字母表
+ */
 public class Print {
-    char ch;
-    void output(int x) {
+    public final static int UPPERCASE = 1;
+    public final static int LOWERCASE = 2;
+    
+    /**
+     * 在控制台输出大小写英文字母表
+     * @param x 决定字母表类型。1为大写，2为小写
+     * @throws IllegalArgumentException("InputXRangeException") 若输入的x值不为1或2
+     */
+    public void output(int x) {
+        char ch;
         if(x == 1) {
             ch = 65;
             while(ch <= 90) {
@@ -11,7 +22,6 @@ public class Print {
             }
             System.out.println("\0");
         }
-        
         else if(x == 2) {
             ch = 97;
             while(ch <= 122) {
@@ -20,7 +30,7 @@ public class Print {
             }
             System.out.println("\0");
         }
-        
-        else System.out.println("Input Number Range Error!");
+        // Exception handling
+        else throw new IllegalArgumentException("InputXRangeException");
     }
 }
--- Homework-2/homework2_6/TestClass.java
+++ Homework-2/homework2_6/TestClass.java
@@ -1,11 +1,14 @@
 package homework2_6;
 
+/**
+ * TestClass类，用于测试Print类
+ */
 public class TestClass {
     public static void main(String[] args) {
         Print capitalAlphabet = new Print();
         Print alphabet = new Print();
 
-        capitalAlphabet.output(1);
-        alphabet.output(2);
+        capitalAlphabet.output(Print.UPPERCASE);
+        alphabet.output(Print.LOWERCASE);
     }
 }
