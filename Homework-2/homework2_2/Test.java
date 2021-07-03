5a9ddcf3b09441feb7a60a281dee1b03ba60efb2
Dozen Prince Li <61978255+dozenprinceli@users.noreply.github.com>
2021-07-03 08:22:58 +0800

    homework2_2更新至v1.2.210703版本

--- Homework-2/homework2_2/Test.java
+++ Homework-2/homework2_2/Test.java
@@ -1,15 +1,18 @@
 package homework2_2;
 
+/**
+ * 测试类Test，用于测试XiYouJiRenWu类
+ */
 public class Test {
     public static void main(String[] args) {
         XiYouJiRenWu sunWuKong = new XiYouJiRenWu();
         XiYouJiRenWu zhuBaJie = new XiYouJiRenWu();
-        
-        sunWuKong.name = "孙悟空";
-        sunWuKong.weapon = "金箍棒";
 
-        zhuBaJie.name = "猪八戒";
-        zhuBaJie.weapon = "九齿钉耙";
+        sunWuKong.setName("孙悟空");
+        sunWuKong.setWeapon("金箍棒");
+
+        zhuBaJie.setName("猪八戒");
+        zhuBaJie.setWeapon("九齿钉耙");
 
         sunWuKong.printName();sunWuKong.printWeapon();
         zhuBaJie.printName();zhuBaJie.printWeapon();
